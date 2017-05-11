89288212ebe3a6bb806e52ff6abc73477d4f2bdc
Oskar Schöldström <public@oxy.fi>
2017-05-11 07:39:23 -0500

    add skip-db option and attempt making dump more stable

--- wp-backup.sh
+++ wp-backup.sh
@@ -16,6 +16,7 @@ args=()
 wp_path=
 customer=
 remote=
+skip_db=
 dirs=()
 
 # Process and remove all flags.
@@ -32,6 +33,8 @@ while (($#)); do
 
     --dir=*) shift; dirs+=("$1") ;;
     --dir) shift; dirs+=("$1") ;;
+
+    --no-db) skip_db=1 ;;
 
     -*) echo "invalid option: $1" >&2; exit 1 ;;
     *) args+=("$1") ;;
@@ -52,37 +55,56 @@ if [ -z "$remote" ]; then
   exit 1
 fi
 
-if [ -z "$wp_path" ]; then
-  wp_path="/home/www/${customer}/deploy/current/web/wp"
+# Unless --skip-db
+if ((!$skip_db)); then
+  if [ -z "$wp_path" ]; then
+    wp_path="/home/www/${customer}/deploy/current/web/wp"
+  fi
+
+  if [ ! -d "$wp_path" ]; then
+    echo "could not find wp installation path." >&2;
+    exit 1
+  fi
+
+  if [ ! -d "$BACKUP_DIR" ]; then
+    mkdir -p $BACKUP_DIR || exit 1
+  fi
+
+  backup_db_file="$BACKUP_DIR/$customer-db-$TIME.sql.gz"
+
+  # Export database
+  tries=0
+  # Make 3 attempts to ensure the file exists and isn't empty.
+  while [ ! -e $backup_db_file -o ! -s $backup_db_file ] && [[ $tries -lt 3 ]]; do
+    $WP_BIN --path="$wp_path" db export - | gzip -f -6 >| $backup_db_file
+    ((tries++))
+  done
+
+  if [[ ! -e $backup_db_file ]] || [[ ! -s $backup_db_file ]]; then
+    echo "database dump failed" >&2
+    exit 1
+  fi
+
+  # Create directory tree.
+  ssh "$remote" mkdir -p ~/backups/$customer/db
+
+  # Backup database
+  $RSYNC_BIN -aqz -e 'ssh' "$backup_db_file" "$remote:~/backups/$customer/db/"
+
+  # Delete files older than 30 days.
+  ssh "$remote" find ~/backups/$customer/db/ -mtime +30 -delete
+
+  # Remove local database dump.
+  rm -f $BACKUP_DB_FILE
 fi
 
-if [ ! -d "$wp_path" ]; then
-  echo "could not find wp installation path." >&2;
-  exit 1
+# If one or multiple --dir
+if [ ${#dirs[@]} -ne 0 ]; then
+  # Create directory tree.
+  ssh "$remote" mkdir -p ~/backups/$customer/db
+
+  # Backup directories.
+  for dir in "${dirs[@]}"; do
+    $RSYNC_BIN -aqz -e 'ssh' --delete "$dir" "$remote:~/backups/$customer"
+  done
 fi
-
-if [ ! -d "$BACKUP_DIR" ]; then
-  mkdir -p $BACKUP_DIR || exit 1
-fi
-
-backup_db_file="$BACKUP_DIR/$customer-db-$TIME.sql.gz"
-
-# Export database
-$WP_BIN --path="$wp_path" db export - | gzip -f -6 >| $backup_db_file
-
-# Create directory tree.
-ssh "$remote" mkdir -p ~/backups/$customer/db
-
-# Backup database
-$RSYNC_BIN -aqz -e 'ssh' "$backup_db_file" "$remote:~/backups/$customer/db/"
-
-# Delete files older than 30 days.
-ssh "$remote" find ~/backups/$customer/db/ -mtime +30 -delete
-
-# Remove local database dump.
-rm -f $BACKUP_DB_FILE
-
-# Backup directories.
-for dir in "${dirs[@]}"; do
-  $RSYNC_BIN -aqz -e 'ssh' --delete "$dir" "$remote:~/backups/$customer"
-done
